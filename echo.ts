8a264763bdb4a1981004d77a6d1175da25977e3e
NEOAPPS <158327205+neoapps-dev@users.noreply.github.com>
2025-07-30 23:57:50 +0300

    Update echo.ts

--- echo.ts
+++ echo.ts
@@ -1,6 +1,6 @@
 import { ModaiTool } from "modai-protocol/dist/tools/base.js";
 import type { ToolMetadata } from "modai-protocol/dist/tools/base.js";
-export class EchoTool extends ModaiTool { // naming convention is [ToolName]Tool
+export default class EchoTool extends ModaiTool {
   metadata: ToolMetadata = {
     name: "echo",
     description: "A simple echo tool.",
